90573f1aef58cbb2f521007f9993f8f6959940e8
UMEZAWA Takeshi <takeshi-umezawa@cybozu.co.jp>
2022-04-26 14:18:44 +0900

    Extend timeout for ACME API
    Signed-off-by: Tomoya Terashima <tomoya-terashima@cybozu.co.jp>

--- pkg/acme/accounts/client.go
+++ pkg/acme/accounts/client.go
@@ -102,7 +102,7 @@ func buildHTTPClientWithCABundle(metrics *metrics.Metrics, skipTLSVerify bool, c
 				TLSClientConfig:       tlsConfig,
 				MaxIdleConns:          100,
 				IdleConnTimeout:       90 * time.Second,
-				TLSHandshakeTimeout:   10 * time.Second,
+				TLSHandshakeTimeout:   20 * time.Second,
 				ExpectContinueTimeout: 1 * time.Second,
 			},
 			Timeout: defaultACMEHTTPTimeout,
